1f180d4de03092256068819c845b9730b26dfc76
_Redstone_c_ <myredstone1024@gmail.com>
2021-01-29 19:12:31 +0800

    Done!

---
+++ Assets/Script/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30.0f;
+    const float minutesToDegrees = -6.0f;
+    const float secondsToDegrees = -6.0f;
+
+    [SerializeField]
+    Transform hoursPivot = default;
+    
+    [SerializeField]
+    Transform minutesPivot = default;
+
+    [SerializeField]
+    Transform secondsPivot = default;
+
+    private void Update()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0.0f, 0.0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0.0f, 0.0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0.0f, 0.0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
