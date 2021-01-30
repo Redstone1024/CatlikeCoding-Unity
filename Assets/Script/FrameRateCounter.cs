8f4b9e2ae2430b8819db7da4a063a083eafeb6fd
_Redstone_c_ <myredstone1024@gmail.com>
2021-01-30 15:51:13 +0800

    From Measuring Performance

---
+++ Assets/Script/FrameRateCounter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI display = default;
+
+    public enum DisplayMode { FPS, MS };
+
+    [SerializeField]
+    DisplayMode displayMode = DisplayMode.FPS;
+
+    [SerializeField, Range(0.1f, 2.0f)]
+    float sampleDuration = 1.0f;
+
+    int frames;
+
+    float duration;
+    float bestDuration = float.MaxValue;
+    float worstDuration = 0.0f;
+
+    private void Update()
+    {
+        var frameDuration = Time.unscaledDeltaTime;
+        frames++;
+        duration += frameDuration;
+
+        if (frameDuration < bestDuration)
+        {
+            bestDuration = frameDuration;
+        }
+        if (frameDuration > worstDuration)
+        {
+            worstDuration = frameDuration;
+        }
+
+        if (duration >= sampleDuration)
+        {
+            if (displayMode == DisplayMode.FPS)
+            {
+                display.SetText(
+                    "FPS\n{0:0}\n{1:0}\n{2:0}",
+                    1.0f / bestDuration,
+                    frames / duration,
+                    1.0f / worstDuration
+                );
+            }
+            else
+            {
+                display.SetText(
+                    "FPS\n{0:1}\n{1:1}\n{2:1}",
+                    1000.0f * bestDuration,
+                    1000.0f * duration / frames,
+                    1000.0f * worstDuration
+                );
+            }
+
+            frames = 0;
+            duration = 0.0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0.0f;
+        }
+    }
+
+}
---
+++ Assets/Script/Graph.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    public enum TransitionMode { Normal, Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode = TransitionMode.Normal;
+
+    [SerializeField, Min(0.0f)]
+    float functionDuration = 1.0f;
+
+    [SerializeField, Min(0.0f)]
+    float transitionDuration = 1.0f;
+
+    Transform[] points;
+
+    float duration = 0.0f;
+
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
+
+    private void Awake()
+    {
+        var step = 2f / resolution;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution * resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+
+    private void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+
+    private void PickNextFunction()
+    {
+        if (transitionMode == TransitionMode.Normal) return;
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
+    private void UpdateFunction()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        var time = Time.time;
+        var step = 2.0f / resolution;
+        var v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    private void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+        }
+    }
+}
