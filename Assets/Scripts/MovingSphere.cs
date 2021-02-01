59d464eac44084d6834b2d3f88406104a140b0f4
_Redstone_c_ <myredstone1024@gmail.com>
2021-02-01 18:26:46 +0800

    Controlling Velocity

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -2,12 +2,25 @@
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)]
+    private float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    private float maxAcceleration = 10f;
+
+    private Vector3 velocity;
+
     private void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
+        var desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        var displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
     }
 }
